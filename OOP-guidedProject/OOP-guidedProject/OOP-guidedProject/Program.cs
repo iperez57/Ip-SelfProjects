46af6f67badcc935143b09acc9609391af45b67a
Isaac Perez <Isaac.Perez.1@outlook.com>
2025-02-19 07:32:46 -0800

    started OOP linkedIn learning course.

---
+++ OOP-guidedProject/OOP-guidedProject/OOP-guidedProject/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Hello, what is your name?");
+
+var name = Console.ReadLine();
+
+if (name == string.Empty)
+    name = "No Name";
+Console.WriteLine($"Welcome {name} to your OOP Adventure!");
